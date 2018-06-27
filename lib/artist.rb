85e6ea192096d0302fa9a95efac584feee22921f
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-06-27 09:48:26 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -8,7 +8,7 @@ class Artist
     @songs = []
   end
 
-  def self.find_or_create_by_name(name)
+  def self.find_or_create_by_name(artist_name)
     found_artist = self.all.find {|artist| artist.name == artist_name}
     if found_artist
       found_artist
